a65df22064feee4a2676956acfc1e5c2719ce189
pouriya-zarbafian <pouriya.zarbafian@gmail.com>
2019-09-02 05:37:53 +0200

    configurable HTTPS

--- src/main/java/backbone/config/HttpsConfig.java
+++ src/main/java/backbone/config/HttpsConfig.java
@@ -4,6 +4,7 @@ import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,10 @@ import org.springframework.context.annotation.Configuration;
 public class HttpsConfig {
 
     @Bean
+    @ConditionalOnProperty(
+            value = "security.https.disabled",
+            havingValue = "false"
+    )
     public ServletWebServerFactory servletContainer() {
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
             @Override
